2c8f788756b31d412442bdd7a8eac69753213ab7
Optimalsten <soa@prg.spb.ru>
2023-10-01 22:39:16 +0300

    feat: add to project HOMEWORK 001(64) 002(66) 003(68) for SEMINAR 9

---
+++ SEM9_HOMEWORK001_64/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 64: Задайте значение N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от N до 1.
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+// МЕТОД 1 - ЗАДАНИЕ натурального N, с контролем допустимости типа значения, с рекурсией
+int GetNumberContr(string message) // conrtNum - True (корректно)/False (некорректно)
+{
+    Console.Write($"\n Задайте число {message}: ");
+    string numberStr = Console.ReadLine();
+
+    if (int.TryParse(numberStr, out int numN))
+    {
+        if (numN > 0) // обработка неположительного значения
+        {
+            return numN;
+        }
+    }
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine($" Заданное значение ({numberStr}) некорректно!");
+    Console.ForegroundColor = ConsoleColor.White;
+    return GetNumberContr(message);
+}
+
+// МЕТОД 2 - ВЫВОД НА ЭКРАН значений от N до 1 (с использ. рекурсии)
+void PrintNewNumb(int numberN)
+{
+    if (numberN > 0)
+    {
+        Console.Write($" {numberN} ");
+        PrintNewNumb(numberN - 1);
+    }
+}
+
+Console.Clear();
+Console.WriteLine();
+
+int numberN = GetNumberContr("- N (целое, натуральное)");
+Console.ForegroundColor = ConsoleColor.Green;
+PrintNewNumb(numberN);
+Console.ForegroundColor = ConsoleColor.White;
+
+// Задержка экрана
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.White;
+Console.Write("\n Для продолжения нажмите любую клавишу...\n"); //  "\n - "возврат каретки"
+Console.Read();
---
+++ SEM9_HOMEWORK002_66/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 66: Задайте значения M и N.
+// Напишите программу, которая найдёт сумму натуральных элементов
+// в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// МЕТОД 1 - задание натуральн.числа, с контролем допустимости типа значения и величины, с рекурсией
+int GetNumberContr(string message, int minNumN = 0)
+{
+    Console.Write($"\n Задайте число {message}: ");
+    string numberStr = Console.ReadLine();
+
+    if (int.TryParse(numberStr, out int numN))
+    {
+        if (numN > minNumN) // обработка корректного значения
+        {
+            return numN;
+        }
+    }
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine($" Заданное значение ({numberStr}) некорректно!");
+    Console.ForegroundColor = ConsoleColor.White;
+    return GetNumberContr(message, minNumN);
+}
+
+// МЕТОД 2 - ВЫВОД НА ЭКРАН значений от M до N (с использ. рекурсии)
+void PrintNewNumb(int numberM, int numberN)
+{
+    if (numberM <= numberN)
+    {
+        Console.Write($" {numberM}\t");
+        PrintNewNumb(numberM + 1, numberN);
+    }
+}
+
+// МЕТОД 3 - Сумма натуральных чисел от M до N (включительно, с использ. рекурсии)
+int SummBetwMN(int M, int N)
+{
+    int sumTemp = 0;
+    if (M <= N)
+    {
+        sumTemp = M + SummBetwMN(M + 1, N);
+    }
+    return sumTemp;
+}
+
+Console.Clear();
+Console.WriteLine();
+
+int numberM = GetNumberContr("- M (целое, натуральное)");
+int numberN = GetNumberContr($"- N (целое, натуральное, которое больше или равно {numberM})", numberM - 1);
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($" Задан ряд натуральных чисел от {numberM} до {numberN}:\n");
+PrintNewNumb(numberM, numberN);
+Console.Write($"\n\n Сумма указанных натуральных чисел равна: ");
+Console.WriteLine(SummBetwMN(numberM, numberN));
+Console.ForegroundColor = ConsoleColor.White; 
+
+// Задержка экрана
+Console.ForegroundColor = ConsoleColor.White;
+Console.Write("\n Для продолжения нажмите любую клавишу...\n"); //  "\n - "возврат каретки"
+Console.Read();
